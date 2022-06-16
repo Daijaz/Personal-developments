80d169e46e9f4ec577a9ccae54603f63795cca7c
Miguel Ángel Osorio Londoño <57534284+Daijaz@users.noreply.github.com>
2022-06-16 17:25:25 -0500

    Create Laboratorio1Activiad2.py

---
+++ Laboratorio1Activiad2.py
@@ -0,0 +1,10 @@
+#El presente código hace parte del laboratorio 1 - actividad 2 de la página de cupi2 de la Universidad de los Andes.
+#Definición de variables.
+variableUno = input("Ingrese la variable 1: ")
+variableDos = input("Ingrese la variable 2: ")
+variableTres = input("Ingrese la variable 3: ")
+
+#Impresiones de pantalla con los desplazamientos requeridos.
+print('El valor de la variable 1 es: ',variableTres)
+print('El valor de la variable 2 es: ',variableUno)
+print('El valor de la variable 3 es: ',variableDos)
